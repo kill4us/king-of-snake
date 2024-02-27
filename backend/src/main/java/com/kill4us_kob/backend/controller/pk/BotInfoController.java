d4c4785d11e4443cdd2c3e1586eeb4f1e2051043
kill4us <1204330998@qq.com>
2024-02-27 21:21:21 +0800

    实现前后端传输数据

--- backend/src/main/java/com/kill4us_kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kill4us_kob/backend/controller/pk/BotInfoController.java
@@ -3,18 +3,19 @@ package com.kill4us_kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public List<String> getBotInfo() {
-        List<String> list = new LinkedList<>();
-        list.add("sword");
-        list.add("tiger");
-        list.add("apple");
-        return list;
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "Apple");
+        bot1.put("rating", "1500");
+        return bot1;
     }
 }
