f01292b8768914956bf468a11bcf857ae19f5978
kill4us <1204330998@qq.com>
2024-03-02 14:29:47 +0800

    实现spring-security

--- backend/src/main/java/com/kill4us_kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kill4us_kob/backend/controller/pk/IndexController.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/pk/")
+
 public class IndexController {
-    @RequestMapping("index/")
+    @RequestMapping("/pk/index/")
     public String index() {
         return "pk/index.html";
     }
