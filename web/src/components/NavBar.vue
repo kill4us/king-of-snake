370693f9ea8f9ade786c14801c17232ac7d67437
kill4us <1204330998@qq.com>
2024-03-03 14:59:23 +0800

    实现登录，登出

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -14,16 +14,29 @@
           <router-link :class="route_name == 'ranklist_index' ? 'nav-link active' : 'nav-link'" :to="{name: 'ranklist_index'}">天梯排行榜</router-link>
         </li>
       </ul>
-      <ul class="navbar-nav">
+      <ul class="navbar-nav" v-if="$store.state.user.is_login == true">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            kill4us
+            {{ $store.state.user.username }}
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">个人中心</a></li>
             <li><router-link class="dropdown-item" :to="{name: 'userbots_index'}">我的Bot</router-link></li>
-            <li><a class="dropdown-item" href="#">退出登录</a></li>
+            <li><hr class="dropdown-diver"></li>
+            <li><a class="dropdown-item" href="#" @click="logout">退出登录</a></li>
           </ul>
+        </li>
+      </ul>
+      <ul class="navbar-nav" v-else>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'account_login'}" role="button" aria-expanded="false">
+            登录
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'account_register'}" role="button" aria-expanded="false">
+            注册
+          </router-link>
         </li>
       </ul>
     </div>
@@ -34,13 +47,20 @@
 <script>
 import { useRoute } from 'vue-router';
 import { computed } from 'vue';
+import {  useStore } from 'vuex';
 
 export default {
     setup() {
         const route = useRoute();
+        const store = useStore();
+        const logout = () => {
+            store.dispatch("logout");
+        };
+
         let route_name = computed(() => route.name);
         return {
-          route_name
+          route_name,
+          logout,
         }
     }
 }
--- web/src/views/user/account/UserAccountLoginView.vue
+++ web/src/views/user/account/UserAccountLoginView.vue
@@ -2,16 +2,16 @@
     <ContentField>
         <div class="row justify-content-md-center">
             <div class="col-3">
-                <form>
+                <form @submit.prevent="login">
                     <div class="mb-3">
                         <label for="username" class="form-label">用户名</label>
-                        <input type="text" class="form-control" id="username" placeholder="输入你的用户名">
+                        <input v-model="username" type="text" class="form-control" id="username" placeholder="输入你的用户名">
                     </div>
                     <div class="mb-3">
                         <label for="username" class="form-label">密码</label>
-                        <input type="password" class="form-control" id="username" placeholder="输入你的密码">
+                        <input v-model="password" type="password" class="form-control" id="password" placeholder="输入你的密码">
                     </div>
-                    <div class = "error-message"></div>
+                    <div class = "error-message">{{ error_message }}</div>
                     <button type="submit" class="btn btn-outline-primary">登录</button>
                 </form>
             </div>
@@ -22,10 +22,47 @@
 
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import { useStore } from 'vuex';
+import { ref } from 'vue';
+import router from '@/router';
+
 
 export default {
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();  //  取出全局变量
+        let username = ref('');
+        let password = ref('');
+        let error_message = ref('');
+
+        const login = () => {  //  触发函数
+            error_message.value = "";
+            store.dispatch("login", {
+                username: username.value,  //  ref取值是.value
+                password: password.value,
+                success() {
+                    store.dispatch("getinfo", {
+                        success() {
+                            router.push({ name: "home"});
+                            console.log(store.state.user);
+                        }
+                    })
+                    
+                },
+                error() {
+                    error_message.value = "用户名或密码错误";
+                }
+            })
+        }
+        return {
+            username,
+            password,
+            error_message,
+            login,
+        }
+
     }
 }
 </script>
